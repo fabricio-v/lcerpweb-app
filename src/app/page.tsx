8b3643ac2c26cc04a244bd492bade5e96b8eb0a0
fabricio-v <fabricio.ventura@lcsistemas.com.br>
2025-01-15 21:18:50 -0300

    correcao do problema de estilização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
             alt="Logo"
           /> */}
           <h1 className="text-2xl text-lc-secondary">
-            Espaço reservado ao marketing 123
+            Espaço reservado ao marketing
           </h1>
         </div>
 

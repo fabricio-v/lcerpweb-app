5c572bf14b0373261a399b2861c594f3010ef7a7
fabricio-v <fabricio.ventura@lcsistemas.com.br>
2025-01-17 16:00:56 -0300

    https api

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,15 +1,13 @@
 import axios from "axios";
 
-// export const CONNECTION_TIMEOUT = 20000;
 export const CONNECTION_TIMEOUT_TEST = 2000;
 export const VERSAO_API = "v1";
 
 const baseURL_DEV = `http://localhost:8080/api/${VERSAO_API}`;
-const baseURL_PROD = `https://143.137.152.19:8080/api/${VERSAO_API}`;
+const baseURL_PROD = `https://api.lcerp.com.br/api/${VERSAO_API}`;
 
 const api = axios.create({
-  baseURL: baseURL_PROD,
-  // timeout: CONNECTION_TIMEOUT,
+  baseURL: process.env.NODE_ENV === "development" ? baseURL_DEV : baseURL_PROD,
 });
 
 export default api;

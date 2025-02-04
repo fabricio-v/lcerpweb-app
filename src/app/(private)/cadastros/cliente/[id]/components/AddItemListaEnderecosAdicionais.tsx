991bc44a2718916d1d072ebb08fc965a40554d44
fabricio-v <fabricio.ventura@lcsistemas.com.br>
2025-02-04 11:20:49 -0300

    refactor: alteração no endereços adiconais do cliente

--- src/app/(private)/cadastros/cliente/[id]/components/AddItemListaEnderecosAdicionais.tsx
+++ src/app/(private)/cadastros/cliente/[id]/components/AddItemListaEnderecosAdicionais.tsx
@@ -29,6 +29,8 @@ function AddItemListaEnderecosAdicionais({
 
   const { dataCidades, loadCidades } = useSearchCidades();
 
+  const [isShowForm, setIsShowForm] = useState(false);
+
   const [id, setId] = useState<number>(0);
   const [descricao, setDescricao] = useState<string>("");
   const [endereco, setEndereco] = useState<string>("");
@@ -101,159 +103,185 @@ function AddItemListaEnderecosAdicionais({
 
   return (
     <div>
-      <div className="items-end pb-4">
-        <div className="flex flex-1 flex-col gap-6 pb-6">
-          <InputWithLabel
-            ref={inputRef}
-            label="Descrição"
-            value={descricao}
-            onChange={(e) => setDescricao(e.target.value)}
-            maxLength={50}
-            placeholder="Ex: Entrega, Cobrança, etc."
-          />
-        </div>
-
-        <div className="flex flex-1 flex-col gap-6 pb-6 md:grid md:grid-cols-[1fr_120px_1fr_200px]">
-          <InputWithLabel
-            label="Endereço"
-            value={endereco}
-            onChange={(e) => setEndereco(e.target.value)}
-            maxLength={100}
-          />
-
-          <InputWithLabel
-            label="Número"
-            value={numero}
-            onChange={(e) => setNumero(e.target.value)}
-            maxLength={10}
-          />
-
-          <InputWithLabel
-            label="Bairro"
-            value={bairro}
-            onChange={(e) => setBairro(e.target.value)}
-            maxLength={50}
-          />
-
-          <CepInput
-            label="CEP"
-            value={cep}
-            onChange={(e) => setCep(e.target.value)}
-          />
-        </div>
-
-        <div className="flex flex-1 flex-col gap-6 md:flex-row">
-          <InputWithLabel
-            label="Referência"
-            value={referencia}
-            onChange={(e) => setReferencia(e.target.value)}
-            maxLength={150}
-          />
-          <div className="flex flex-1 md:max-w-[250px]">
-            <Combobox
-              label="Estado"
-              data={estados.map((item) => {
-                return {
-                  label: item.uf + " - " + item.nome,
-                  value: item.id + "",
-                };
-              })}
-              valueSelected={estadoSel}
-              onChangeValueSelected={setEstadoSel}
-            />
+      {enderecosAdicionaisLista.length > 0 && (
+        <>
+          <div className="flex flex-1 flex-col gap-4 py-4">
+            {enderecosAdicionaisLista.map((item, index) => (
+              <ItemListaEnderecosAdicionais
+                item={item}
+                key={index}
+                onRemove={onRemove}
+                onEdit={(item) => {
+                  setIsShowForm(true);
+                  setId(item.id);
+                  setDescricao(item.descricao);
+                  setCep(item.cep);
+                  setEndereco(item.endereco);
+                  setNumero(item.numero);
+                  setBairro(item.bairro);
+                  setReferencia(item.referencia);
+                  setEstadoSel(item.estado.id + "");
+                  setCidadeSel(item.cidade.id + "");
+                  setCidadeSelNome(item.cidade.nome);
+                  setEstadoSelUf(item.estado.uf);
+                }}
+              />
+            ))}
           </div>
+          <Separator className="mb-4" />
+        </>
+      )}
 
-          <div className="flex flex-1 md:max-w-[500px]">
-            <Combobox
-              label="Cidade"
-              data={dataCidades.cidades.map((item) => {
-                return {
-                  label: item.nome,
-                  value: item.id + "",
-                };
-              })}
-              valueSelected={cidadeSel}
-              onChangeValueSelected={setCidadeSel}
-            />
-          </div>
-        </div>
-
-        <div className="mt-6 flex flex-col-reverse justify-end gap-2 md:flex-row">
+      {!isShowForm ? (
+        <div className="flex items-center justify-center">
           <Button
             type="button"
-            variant={"ghost"}
-            className={isMobile ? "w-full" : ""}
-            info="Limpar dados do endereço"
-            onClick={limparFormulario}
-          >
-            Limpar
-          </Button>
-
-          <Button
-            type="button"
-            // size={isMobile ? "default" : "icon"}
-            variant={"outline"}
-            // className={isMobile ? "w-full" : ""}
+            variant={"secondary"}
             info="Adicionar endereço"
             onClick={() => {
-              if (validate()) {
-                onAdd({
-                  id: id > 0 ? id : new Date().getTime(),
-                  descricao,
-                  cep,
-                  cidade: {
-                    id: Number(cidadeSel),
-                    nome: cidadeSelNome,
+              setIsShowForm(true);
+              setTimeout(() => {
+                inputRef.current?.focus();
+              }, 1);
+            }}
+          >
+            <Plus />
+            Novo endereço
+          </Button>
+        </div>
+      ) : (
+        <div className="items-end">
+          <div className="flex flex-1 flex-col gap-6 pb-6">
+            <InputWithLabel
+              ref={inputRef}
+              label="Descrição"
+              value={descricao}
+              onChange={(e) => setDescricao(e.target.value)}
+              maxLength={50}
+              placeholder="Ex: Entrega, Cobrança, etc."
+            />
+          </div>
+
+          <div className="flex flex-1 flex-col gap-6 pb-6 md:grid md:grid-cols-[1fr_120px_1fr_200px]">
+            <InputWithLabel
+              label="Endereço"
+              value={endereco}
+              onChange={(e) => setEndereco(e.target.value)}
+              maxLength={100}
+            />
+
+            <InputWithLabel
+              label="Número"
+              value={numero}
+              onChange={(e) => setNumero(e.target.value)}
+              maxLength={10}
+            />
+
+            <InputWithLabel
+              label="Bairro"
+              value={bairro}
+              onChange={(e) => setBairro(e.target.value)}
+              maxLength={50}
+            />
+
+            <CepInput
+              label="CEP"
+              value={cep}
+              onChange={(e) => setCep(e.target.value)}
+            />
+          </div>
+
+          <div className="flex flex-1 flex-col gap-6 md:flex-row">
+            <InputWithLabel
+              label="Referência"
+              value={referencia}
+              onChange={(e) => setReferencia(e.target.value)}
+              maxLength={150}
+            />
+            <div className="flex flex-1 md:max-w-[250px]">
+              <Combobox
+                label="Estado"
+                data={estados.map((item) => {
+                  return {
+                    label: item.uf + " - " + item.nome,
+                    value: item.id + "",
+                  };
+                })}
+                valueSelected={estadoSel}
+                onChangeValueSelected={setEstadoSel}
+              />
+            </div>
+
+            <div className="flex flex-1 md:max-w-[500px]">
+              <Combobox
+                label="Cidade"
+                data={dataCidades.cidades.map((item) => {
+                  return {
+                    label: item.nome,
+                    value: item.id + "",
+                  };
+                })}
+                valueSelected={cidadeSel}
+                onChangeValueSelected={setCidadeSel}
+              />
+            </div>
+          </div>
+
+          <div className="mt-6 flex flex-col-reverse justify-end gap-2 md:flex-row">
+            <Button
+              type="button"
+              variant={"ghost"}
+              className={isMobile ? "w-full" : ""}
+              info="Limpar dados do endereço"
+              onClick={() => {
+                limparFormulario();
+                setIsShowForm(false);
+              }}
+            >
+              Cancelar
+            </Button>
+
+            <Button
+              type="button"
+              variant={"secondary"}
+              info="Adicionar"
+              onClick={() => {
+                if (validate()) {
+                  onAdd({
+                    id: id > 0 ? id : new Date().getTime(),
+                    descricao,
+                    cep,
+                    cidade: {
+                      id: Number(cidadeSel),
+                      nome: cidadeSelNome,
+                      estado: {
+                        id: Number(estadoSel),
+                        uf: estadoSelUf,
+                        nome: "",
+                      },
+                    },
                     estado: {
                       id: Number(estadoSel),
                       uf: estadoSelUf,
                       nome: "",
                     },
-                  },
-                  estado: {
-                    id: Number(estadoSel),
-                    uf: estadoSelUf,
-                    nome: "",
-                  },
-                  endereco,
-                  numero,
-                  bairro,
-                  referencia,
-                });
-              }
-              limparFormulario();
-            }}
-          >
-            <Plus />
-            Adicionar endereço
-          </Button>
+                    endereco,
+                    numero,
+                    bairro,
+                    referencia,
+                  });
+
+                  limparFormulario();
+                  setIsShowForm(false);
+                }
+              }}
+            >
+              {/* <Plus /> */}
+              Adicionar
+            </Button>
+          </div>
         </div>
-      </div>
-
-      <Separator />
-
-      <div className="flex flex-1 flex-col gap-4 py-4">
-        {enderecosAdicionaisLista.map((item, index) => (
-          <ItemListaEnderecosAdicionais
-            item={item}
-            key={index}
-            onRemove={onRemove}
-            onEdit={(item) => {
-              setId(item.id);
-              setDescricao(item.descricao);
-              setCep(item.cep);
-              setEndereco(item.endereco);
-              setNumero(item.numero);
-              setBairro(item.bairro);
-              setReferencia(item.referencia);
-              setEstadoSel(item.estado.id + "");
-              setCidadeSel(item.cidade.id + "");
-              setCidadeSelNome(item.cidade.nome);
-              setEstadoSelUf(item.estado.uf);
-            }}
-          />
-        ))}
-      </div>
+      )}
     </div>
   );
 }
